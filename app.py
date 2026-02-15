f4661a6f9ef2dca9cc4c5ff500a2e1f12450d59a
Naga Venkata Sairam Yanamadala <ynvsr.25@gmail.com>
2026-02-15 18:36:19 -0500

    sqlite, sqlalchemy and one-to-many relation

--- app.py
+++ app.py
@@ -1,20 +1,37 @@
 from flask import Flask, request
 from flask_smorest import Api
+import os
+
+from db import db
+import models
 
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 
-app = Flask(__name__)
+def create_app(db_url=None):
 
-app.config["PROPAGATE_EXCEPTIONS"] = True
-app.config["API_TITLE"] = "Stores REST API"
-app.config["API_VERSION"] = "v1"
-app.config["OPENAPI_VERSION"] = "3.0.3"
-app.config["OPENAPI_URL_PREFIX"] = "/"
-app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+  app = Flask(__name__)
 
-api = Api(app)
+  app.config["PROPAGATE_EXCEPTIONS"] = True
+  app.config["API_TITLE"] = "Stores REST API"
+  app.config["API_VERSION"] = "v1"
+  app.config["OPENAPI_VERSION"] = "3.0.3"
+  app.config["OPENAPI_URL_PREFIX"] = "/"
+  app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+  app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+  app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+  app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+  db.init_app(app)
+  api = Api(app)
 
-api.register_blueprint(ItemBlueprint)
-api.register_blueprint(StoreBlueprint)
+  print("App created")
+  print("Database URI:", app.config["SQLALCHEMY_DATABASE_URI"])
+
+  with app.app_context():
+        print("Inside app context, creating tables...")
+        db.create_all()
+
+  api.register_blueprint(ItemBlueprint)
+  api.register_blueprint(StoreBlueprint)
+
+  return app
--- resources/store.py
+++ resources/store.py
@@ -3,31 +3,47 @@ from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from schemas import StoreSchema
-from db import stores
+from models import StoreModel
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+from db import db
 
 blp = Blueprint("stores", __name__, description="Operations on stores")
 
 @blp.route("/store/<string:store_id>")
 class Store(MethodView):
-  @blp.response(200, StoreSchema(many=True))
+  @blp.response(200, StoreSchema())
   def get(self, store_id):
-    return stores.values()
+    item = StoreModel.query.get(store_id)
+
+    return item
+  
+  def delete(self, store_id):
+    item = StoreModel.query.get_or_404(store_id)
+
+    db.session.delete(item)
+    db.session.commit()
+
+    return {"message": "store deleted"}
 
 @blp.route("/store")
 class StoreNoId(MethodView):
+  @blp.arguments(StoreSchema)
   @blp.response(200, StoreSchema)
-  def post(self):
-    data = request.get_json()
+  def post(self, store_data):
+    store = StoreModel(**store_data)
+    try:
+      db.session.add(store)
+      db.session.commit()
+    except IntegrityError:
+      abort(
+        400,
+        message = "A store with that name already exists"
+      )
+    except SQLAlchemyError:
+      abort(500, message="An error occured creating the store.")
 
-    if "name" not in data:
-      abort(400, message="name not included in the JSON payload")
-
-    for store in stores.values():
-      if data["name"] ==store["name"]:
-          abort(400, message = "store already exists")
-
-    store_id = uuid.uuid4().hex
-    new_store = {**data, "id": store_id}
-    stores[store_id] = new_store
-
-    return new_store, 201
+    return store
+  
+  @blp.response(200, StoreSchema(many=True))
+  def get(self):
+    return StoreModel.query.all()
